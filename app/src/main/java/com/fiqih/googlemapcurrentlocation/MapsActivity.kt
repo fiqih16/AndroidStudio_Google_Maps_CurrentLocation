d214ff0bc19a887ced9a2fc684ff838c5c6d482b
PBK11 <fiqih1666@gmail.com>
2021-05-05 14:19:55 +0700

    first init

--- app/src/main/java/com/fiqih/googlemapcurrentlocation/MapsActivity.kt
+++ app/src/main/java/com/fiqih/googlemapcurrentlocation/MapsActivity.kt
@@ -29,16 +29,6 @@ class MapsActivity : AppCompatActivity() {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         getCurrentLocation()
     }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @SuppressLint("RestrictedApi")
     fun getCurrentLocation(){
         val mapFragment = supportFragmentManager
